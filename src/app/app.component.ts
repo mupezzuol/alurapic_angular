1c5bc0e510fd3dc41cba3bdf78aa9446fbb5e952
Murillo Pezzuol <murillo.pezzuol@kaspper.com>
2019-07-18 17:28:00 -0300

    Utilizando Diretiva ngFor; Iteracao com DataBinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Alurapic';
+
+  //Array de objetos de Photos
+  photos = [
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
+      description: 'Leão'
+    },
+    {
+      url: 'http://s2.glbimg.com/2xHkvt4IGBYY6ZnkyrrIzGNATyQ=/s.glbimg.com/jo/g1/f/original/2013/09/17/tigre.jpg',
+      description: 'Tigre'
+    }
+  ];
+
 }
