49322f9ab0a56938f8cc5016f0071b3610402504
Murillo Pezzuol <murillo.pezzuol@kaspper.com>
2019-07-18 16:45:47 -0300

    Criando Component; ngModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
+//Convenção do Angular é adc imports referente ao Angular em grupo e depois os imports da aplicação
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
+//Imports da Aplicação
+import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoComponent
   ],
   imports: [
     BrowserModule
