3e083c11f89418efa5435ba84f0b8417e5b8e9e3
Murillo Pezzuol <murillo.pezzuol@kaspper.com>
2019-07-19 17:34:24 -0300

    Ciclo de vida de um componente; Ajuste imports do módulo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PhotoService } from './photos/photo/photo.service';
 
 //Component -> É um decorator, nele configuramos 'metadata' que determina como componente irá ser processado, instanciado, usado etc...
@@ -7,19 +7,24 @@ import { PhotoService } from './photos/photo/photo.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   //Array do tipo Object recebendo vazio
   photos: Object[] = [];
 
-  constructor(photoService: PhotoService){
+  //Constructors -> Usado para injeção de dependencia
+  constructor(private photoService: PhotoService) {
 
-    photoService.listFromUser('flavio')
+  }
+
+  //ngOnInit -> Usado para inicialização (usamos a interface 'OnInit' para auxiliar)
+  ngOnInit(): void {
+    
+    this.photoService.listFromUser('flavio')
       .subscribe(photos => {
         console.log(photos[0].userId);
         this.photos = photos
-      },);
-
+      });
   }
 
 
