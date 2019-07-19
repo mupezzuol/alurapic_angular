a5ec6965f2eda4e85298c94d10fb0652e39173a9
Murillo Pezzuol <murillo.pezzuol@kaspper.com>
2019-07-19 14:58:12 -0300

    Adc Provider do HttpClient em nosso Module principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 //Component -> É um decorator, nele configuramos 'metadata' que determina como componente irá ser processado, instanciado, usado etc...
 @Component({
@@ -9,15 +10,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   //Array de objetos de Photos
-  photos = [
-    {
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
-      description: 'Leão'
-    },
-    {
-      url: 'http://s2.glbimg.com/2xHkvt4IGBYY6ZnkyrrIzGNATyQ=/s.glbimg.com/jo/g1/f/original/2013/09/17/tigre.jpg',
-      description: 'Tigre'
-    }
-  ];
+  photos = [];
+
+  constructor(http: HttpClient){
+    console.log(http);
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http'
 
 //Imports da Aplicação
 import { PhotosModule } from './photos/photos.module';
@@ -12,7 +13,8 @@ import { PhotosModule } from './photos/photos.module';
   ],
   imports: [
     BrowserModule,
-    PhotosModule
+    PhotosModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
