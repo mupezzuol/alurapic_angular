9a9e779e8b57a3ff4f0d9f8d14a763030dde316b
Murillo Pezzuol <murillo.pezzuol@kaspper.com>
2019-07-19 18:13:39 -0300

    CommonModule; Importando módulo de browser para modulos comuns

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoComponent } from "./photo/photo.component";
@@ -9,7 +10,10 @@ import { PhotoComponent } from "./photo/photo.component";
     declarations: [ 
         PhotoComponent, 
         PhotoListComponent ],
-    imports: [ HttpClientModule ]
+    imports: [ 
+        HttpClientModule,
+        CommonModule
+    ]
 })
 export class PhotosModule{
 
