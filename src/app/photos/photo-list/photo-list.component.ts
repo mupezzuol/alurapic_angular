ef8a68e9c807cf8f0d9f908baeede226c833a14c
Murillo Pezzuol <murillo.pezzuol@kaspper.com>
2019-07-22 11:19:31 -0300

    Pegando Params das Rotas para os component

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { PhotoService } from '../photo/photo.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-photo-list',
@@ -9,21 +10,25 @@ import { PhotoService } from '../photo/photo.service';
 })
 export class PhotoListComponent implements OnInit {
 
-//Array do tipo Object recebendo vazio
-photos: Object[] = [];
+  //Array do tipo Object recebendo vazio
+  photos: Object[] = [];
 
-//Constructors -> Usado para injeção de dependencia
-constructor(private photoService: PhotoService) {
+  //Constructors -> Usado para injeção de dependencia
+  constructor(
+    private photoService: PhotoService,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
-}
+  //ngOnInit -> Usado para inicialização (usamos a interface 'OnInit' para auxiliar)
+  ngOnInit(): void {
 
-//ngOnInit -> Usado para inicialização (usamos a interface 'OnInit' para auxiliar)
-ngOnInit(): void {
-  
-  this.photoService.listFromUser('flavio')
-    .subscribe(photos => {
-      this.photos = photos
-    });
-}
+    //Atribuo meu parametro passado na URL/Rota em uma variavel
+    const userName = this.activatedRoute.snapshot.params.userName;
+
+    this.photoService.listFromUser(userName)
+      .subscribe(photos => {
+        this.photos = photos
+      });
+  }
 
 }
