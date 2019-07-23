a1d20668c4d150354d065d33e6034f66b1c4ad2e
Murillo Pezzuol <murillo.pezzuol@kaspper.com>
2019-07-23 14:13:49 -0300

    Removendo Importacoes

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
-import { FilterByDescription } from './photo-list/filter-by-description.pipe';
 import { PhotoModule } from './photo/photo.module';
 import { PhotoFormModule } from './photo-form/photo-form.module';
 import { PhotoListModule } from './photo-list/photo-list.module';
-import { HttpClientModule } from '@angular/common/http';
 
 //Declarando um novo módulo
 @NgModule({
