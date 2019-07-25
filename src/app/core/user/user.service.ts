25a5fb65ff002bc90d1dfd8d6299e1e42bee8cf2
Murillo Pezzuol <murillo.pezzuol@kaspper.com>
2019-07-25 14:19:59 -0300

    Comentarios

--- src/app/core/user/user.service.ts
+++ src/app/core/user/user.service.ts
@@ -13,7 +13,7 @@ export class UserService{
     private userSubject = new Subject<User>();
 
     constructor(private tokenService: TokenService){
-        //Quando o serviço for chamado ele irá validar se tem token, se tiver token é pq está LOGADO, portanto ele irá DESCODIFICAR o Token e resgatar o User
+        //Quando o serviço for chamado ele irá validar se tem token, se tiver token é pq está LOGADO, portanto ele irá descriptografar o Token e resgatar o User
         this.tokenService.hasToken() &&
             this.decodeAndNotify();
     }
@@ -21,15 +21,15 @@ export class UserService{
     private decodeAndNotify() {
         const token = this.tokenService.getToken();//Resgato o Token
         
-        // aqui a chamada 'jwt_decode' que descodifica nosso token, onde nele teremos informações de usuário
-        //Como já sabemos as informações que vem do token, nós criamos uma interface de usuário, e na chamada nós já fazemos um CAST de User (as)
+        // aqui a chamada 'jwt_decode' que descriptografa nosso token, onde nele teremos informações de usuário
+        //Como já sabemos as informações que do Payload que descriptografamos, nós criamos uma interface de usuário, e na chamada nós já fazemos um CAST de User (as)
         const user = jwt_decode(token) as User;
         this.userSubject.next(user);
     }
     
     setToken(token: string) {
         this.tokenService.setToken(token);
-        this.decodeAndNotify();//Tenho o token em mãos, vou descodifica-lo
+        this.decodeAndNotify();//Tenho o token em mãos, vou descriptografa-lo
     }
 
     //Retorno u usuário que está em Subject como um Observable (posso utilizar o subscribe)
