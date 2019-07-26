f600e9da6156a008cc776f661bf87b14cdb2a779
Murillo Pezzuol <murillo.pezzuol@kaspper.com>
2019-07-26 11:55:32 -0300

    Validando Form de Registro

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,8 +1,48 @@
-import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     templateUrl: './signup.component.html'
 })
-export class SignUpComponent{
+export class SignUpComponent implements OnInit{
+    
+    signupForm: FormGroup;
+    
+    constructor(private formBuilder: FormBuilder){ }
+    
+    ngOnInit(): void {
+
+        this.signupForm = this.formBuilder.group({
+            email: ['',
+                [
+                    Validators.required,
+                    Validators.email
+                ]
+            ],
+            fullName: ['',
+                [
+                    Validators.required,
+                    Validators.minLength(2),
+                    Validators.maxLength(40)
+                ]
+            ],
+            userName: ['',
+                [
+                    Validators.required,
+                    Validators.pattern(/^[a-z0-9_\-]+$/), //Expressão regular para aceitar somente: deve começar com letra minuscula, e não pode iniciar com numero, mas no final pode
+                    Validators.minLength(2),
+                    Validators.maxLength(30)
+                ]
+            ],
+            password: ['',
+                [
+                    Validators.required,
+                    Validators.minLength(8),
+                    Validators.maxLength(14)
+                ] 
+            ]
+        });
+
+    }
 
 }   
