49676eef0c59488173e22a2abd2948f645d168b3
Murillo Pezzuol <murillo.pezzuol@kaspper.com>
2019-07-29 15:40:19 -0300

    Criando Form; Usamos FormsModule para nao gerar erro

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
-import { PhotoFormComponent } from './photo-form.component';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup, FormsModule } from '@angular/forms';
+
+import { PhotoFormComponent } from './photo-form.component';
+import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
 
 @NgModule({
     declarations: [
         PhotoFormComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        ReactiveFormsModule,
+        VMessageModule,
+        FormsModule
     ],
     exports:[
         PhotoFormComponent
