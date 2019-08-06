b8f48ce43595512383e9b6725c04df5a33b59176
Murillo Pezzuol <murillo.pezzuol@kaspper.com>
2019-08-06 11:27:06 -0300

    Exibindo o progresso; PercentDone

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -1,8 +1,10 @@
-import { UserService } from './../../core/user/user.service';
+import { HttpProgressEvent, HttpEvent, HttpEventType, HttpResponse } from '@angular/common/http';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { finalize } from 'rxjs/operators';
 
+import { UserService } from './../../core/user/user.service';
 import { PhotoService } from './../photo/photo.service';
 import { AlertService } from 'src/app/shared/components/alert/alert.service';
 
@@ -16,6 +18,7 @@ export class PhotoFormComponent implements OnInit {
   photoForm: FormGroup;
   file: File;
   preview: string;//Receberá a foto no formato base64 para apresentar como preview
+  percentDone = 0;
 
   constructor(
       private formBuilder: FormBuilder,
@@ -36,12 +39,33 @@ export class PhotoFormComponent implements OnInit {
     const desc = this.photoForm.get('description').value;//Pegando valor solto
     const allowComments = this.photoForm.get('allowComments').value;//Pegando valor solto
 
+    //pipe + finalize() -> Ele será um código que será exetuado, dando certo ou erro, ele sempre executará isso,
+    // como a navegação é a mesma tanto para erro ou acerto, jogamos aqui o codigo
+    //Quando nós usamos '(event: HttpEvent<any>)' nós conseguimos pegar o HttpEventType, onde temos o progresso de upload do arquivos etc...
+    //Com isso a requisição do corpo será chamado a cada percentual novo etc... portanto usamos alguma condições para tratar isso
+    //Verificamos se o progresso ainda continua, se sim a gnt cria uma forma de calcualr percentual etc..
+    //Se o evento for igual a Response, é pq terminou e finalizou a chamada..
     this.photoService
       .upload(desc, allowComments, this.file)
-      .subscribe(() => {
-          this.alertService.success('Upload complete', true);
-          this.router.navigate(['/user', this.userService.getUserName()])
-      });
+      .pipe(finalize( () => {
+        this.router.navigate(['/user', this.userService.getUserName()])//Jogo o usuário para timeline dele
+      }))
+      .subscribe(
+        (event: HttpEvent<any>) => {
+          //A cada interação eu vou calcular a %
+          //Se o upload for distribuido em 25%, será efetuado 4 chamadas nesse IF, portanto fará 25%, 50% etc.. até finalizar e cair no 'Response'
+          if (event.type == HttpEventType.UploadProgress){
+            this.percentDone = Math.round(100 * event.loaded / event.total); //Calculando o percentual do Progress da requisição
+
+          } else if (event instanceof HttpResponse){//Se o evento atual for a mesma instnacia de HttpResponse é pq temos uma resposta, ou seja o progress acabou
+            //Aqui é quando acabou o progress e eu obtive a resposta
+            this.alertService.success('Upload complete', true);
+          }
+        },
+        err => {
+          console.log(err);
+          this.alertService.danger('Upload Error!', true);
+        });
 
   }
 
