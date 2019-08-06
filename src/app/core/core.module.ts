5acc55ccb47a551c029552a31e52e2a5a6840972
Murillo Pezzuol <murillo.pezzuol@kaspper.com>
2019-08-06 17:18:07 -0300

    Criando LoadingComponent; Fazendo valid

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -7,6 +7,7 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FooterComponent } from './footer/footer.component';
 import { RequestInterceptor } from './auth/request.interceptor';
 import { HeaderComponent } from './header/header.component';
+import { LoadingModule } from '../shared/components/loading/loading.module';
 // providers -> Adicono um Objeto dizendo sobre qual INTERCEPTADOR ele usará nas requisições, nesse caso eu passo a classe customizada 'RequestInterceptor'
 // multi -> é que se tiver outro INTERCEPTADOR ele poderá seguir em frente
 @NgModule({
@@ -17,7 +18,8 @@ import { HeaderComponent } from './header/header.component';
     imports: [
         CommonModule,
         RouterModule,
-        AlertModule
+        AlertModule,
+        LoadingModule
     ],
     exports: [
         HeaderComponent,
--- src/app/shared/components/loading/loading.component.ts
+++ src/app/shared/components/loading/loading.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { LoadingService } from './loading.service';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LoadingType } from './loading-type';
+import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'ap-loading',
-    templateUrl: './loading.component.html',
-    styleUrls: ['./loading.compoennt.css']
+    templateUrl: './loading.component.html'
+   // styleUrls: ['./loading.compoennt.css']
 })
-export class LoadingComponent{
+export class LoadingComponent implements OnInit{
 
+    //Lembre-se que qnd tem '$' é pq é um Observable
+    loading$: Observable<string>;//String pois nós convertemos com MAP o valor do ENUM para adicionar na class do HTML
+    
+    constructor(private loadingService: LoadingService) { }
+    
+    ngOnInit(): void {
+        
+        this.loadingService
+            .getLoading()
+            .pipe(map( loadingType => loadingType.valueOf()));//Convertemos com 'map()' oq vem de resposta para String com 'valueOf'.. Será ou 'loading' ou 'stopped'
+
+    }
+    
 }
