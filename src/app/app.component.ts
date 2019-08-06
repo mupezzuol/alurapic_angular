625e9d38a0840b7be46a0bdef565dec30f6e80ad
Murillo Pezzuol <murillo.pezzuol@kaspper.com>
2019-08-06 10:33:25 -0300

    Alterando title das paginas; Interagindo com o sistema de rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { filter, map, switchMap } from 'rxjs/operators';
 
 //Component -> É um decorator, nele configuramos 'metadata' que determina como componente irá ser processado, instanciado, usado etc...
 @Component({
@@ -6,6 +9,27 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  
+export class AppComponent implements OnInit {
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private titleService: Title) { }
+
+    
+  ngOnInit(): void {
+
+    //Aplicando regra para pegar o 'TITLE' das Rotas e colocar no Title das páginas
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .pipe(map(() => this.activatedRoute))
+      .pipe(map(route => {
+        while (route.firstChild) route = route.firstChild;
+        return route;
+      }))
+      .pipe(switchMap(route => route.data))
+      .subscribe(event => this.titleService.setTitle(event.title));
+
+  }
+
 }
--- src/app/home/home.rounting.module.ts
+++ src/app/home/home.rounting.module.ts
@@ -20,10 +20,16 @@ const routes: Routes = [
             {
                 path: '',
                 component: SignInComponent,
+                data: { 
+                    title: 'Sign in'
+                }
             },
             {
                 path: 'signup',
                 component: SignUpComponent,
+                data: { 
+                    title: 'Sign up'
+                }
             }
         ]
     }
