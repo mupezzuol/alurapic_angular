c48ac2d73c85262b8c760131fa321323a8042f95
Murillo Pezzuol <murillo.pezzuol@kaspper.com>
2019-08-07 14:35:07 -0300

    StackTrace padronizado entre navegadores

--- src/app/errors/global-error-handler/global-error-handler.ts
+++ src/app/errors/global-error-handler/global-error-handler.ts
@@ -1,12 +1,30 @@
 import { ErrorHandler } from '@angular/core';
+import * as StackTrace from 'stacktrace-js';
 
 export class GlobalErrorHandler implements ErrorHandler{
     
     handleError(error: any): void {
         
-        console.log('Passei pelo Global Error Handler');
+        console.log('Global Error Handler');
+
+        //StackTrace -> biblioteca que nos ajuda a mostrar o mesmo erro em todos os tipos de navegadores diferentes
+        //fromError -> Instancia de erro, dentro dela eu recebo um array, cada item do array é 'frame' e eu pego chamando 'then()' pois é um promise
+        //stackFrames -> é o retono do THEN, ou seja, meu array, cada item dele é um frame com os erros etc... porém queremos trata-los para ficar mais visivel
+        StackTrace
+            .fromError(error)
+            .then(stackFrames => {
+
+                //Eu faço um MAP nesse ARRAY retornado pelo then();
+                //Eu uso o MAP para converter esse item em string, depois no final eu faço um 'join('\n')' que ao final da string convertida ele pula uma linha
+                const stackAsString = stackFrames
+                    .map(sf => sf.toString())
+                    .join('\n');
+
+                //Printo minha String montado de acordo com os 'stackFrames'
+                console.log(stackAsString);
+
+            });
         
-        throw error;//Deixo estorar o erro
     }
 
 }
